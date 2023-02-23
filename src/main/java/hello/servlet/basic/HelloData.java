a7ee694066b5b10a49c0fb8692b1ccc397680d88
yujeong-shin <vouobb@naver.com>
2023-02-23 18:47:07 +0900

    HTTP Servlet Request(GET 쿼리 파라미터, POST HTML form, API 메시지 바디(단순 텍스트, JSON), Response(단순 텍스트, HTML, API JSON)

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,10 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter //lombok 라이브러리
+public class HelloData {
+    private String username;
+    private int age;
+}
